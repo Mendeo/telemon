e597503cf988c60a54d88787820c5c017d8a4cc1
Themen <thesolve@mail.ru>
2024-11-20 09:40:55 +0300

    Исправление опечатки.

--- main.mjs
+++ main.mjs
@@ -47,7 +47,7 @@ command_watcher(
 		command: 'vcgencmd',
 		args: ['measure_temp'],
 		period: 30000,
-		header: 'Температура процессора превысила 65 градусов!',
+		header: 'Температура процессора превысила 70 градусов!',
 		timeout: 1800000,
 		pre: middlewares.parseRPItemp,
 		trigger: (t) => Number(t) >= 70,
